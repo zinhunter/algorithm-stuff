0f1b2054ba9fb20cffb6a16bda611961b6561938
Ayrton <54418646+zinhunter@users.noreply.github.com>
2021-01-04 09:45:09 -0600

    Add files via upload
    Added Reverse a String code

---
+++ Cpp/reverseString.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <boost/range/adaptor/reversed.hpp>
+#include <bits/stdc++.h> 
+
+auto strReverser(std::string &strToReverse){
+  //Time complexity O(n), we iterate through the whole string
+  for(int i = strToReverse.length() - 1; i >= 0; i--){
+    std::cout << strToReverse[i];
+  }
+  std::cout << "\n";
+}
+
+auto strReverserRbLoop(std::string &strToReverse){
+  //Using a reversed range based loop... basically the same as the first one, needs boost.
+  //Time complexity O(n), we iterate through the whole string
+  for(auto i : boost::adaptors::reverse(strToReverse)){
+    std::cout << i;
+  }
+  std::cout << "\n";
+}
+
+int main() {
+  std::string myString;
+  std::getline(std::cin,myString);
+  strReverser(myString);
+  strReverserRbLoop(myString);
+
+  //Simplest one, needs bits
+  /*Time complexity O(n/2), depending on the number of swaps, so O(n)*/
+  reverse(myString.begin(),myString.end());
+  std::cout << myString << "\n";
+}
